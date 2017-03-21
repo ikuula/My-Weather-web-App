4a13bb3fae06aef89f18b2c5a8f8b9fb99c23c3d
Isaac Kuula <ikuula90@gmail.com>
2017-03-20 18:03:33 -0700

    tried to bring some codepen stuff in
    Sidr not working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,66 @@
-// http://simpleweatherjs.com/
-
 // https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+// https://cdnjs.cloudflare.com/ajax/libs/sidr/2.2.1/jquery.sidr.min.js
 
-$.simpleWeather({
-  location: 99203,
+// call Sidr Plugin. Toggle by Default
+$('#sidr-show-hide').sidr();
+
+// Close from inside
+$('#close').sidr({
+  method: 'close'
+})
+
+// Get and store Geo Location lat/long coordinates
+if ('geolocation' in navigator) {
+
+   $('.geo').show(); 
+
+} else {
   
-  unit: 'f',
-  
-  success: function(weather){
-    // Check for weather
-    console.log(weather);
-    
-    // Display Current Weather
-    $('.temp').text(weather.temp);
-    $('.city').text(weather.city);
-    $('.state').text(weather.region);
-    $('.image img').attr('src', weather.image);
-    
-   // Display Future Weather
-    $('.forecast figure:nth-child(1) img').attr('src', weather.forecast[0].image);
-    $('.forecast figure:nth-child(1) figcaption').text(weather.forecast[0].text + ' baby!');
-  
-  },
-  error: function(error) {
-    console.log('Look outside.');
-  }
+  $('.geo').hide();
+  $('.geo').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+// On Click, Get Geolocation, Call Weather Function
+$('.geo').click( function() {
+      
+    //load weather using your lat/long coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+      
+      // Check lat/long coordinates
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      
+      console.log(lat, long);
+      
+      // Send to SimpleWeather
+      getWeather( lat + ',' + long ); 
+    });
+   
 });
+
+// Get Weather
+var getWeather = function(location) {
+  
+    $.simpleWeather({
+    location: location,
+    unit: 'f',
+    success: function(weather) {
+      
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('img').attr('src', weather.image);
+        
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look Outside.');
+    }
+  
+  });
+  
+};
