3a85d4fcb29b96c042fbf7380decddb42ac38f42
Isaac Kuula <ikuula90@gmail.com>
2017-03-21 15:24:59 -0700

    added forecast to Spokane
    now just need to pull everything else over to the other location pages

--- js/scripts.js
+++ js/scripts.js
@@ -80,12 +80,53 @@ $.simpleWeather({
       $('#spokane .temp').text(weather.temp);
       $('#spokane .city').text(weather.city);
       $('#spokane img').attr('src', weather.image);
-      
+	  $('#spokane .today').text(weather.forecast[0].day + ', ' + weather.forecast[0].date);
+	
+		// Display Data : Day 1ß
+    $('#spokane figure:nth-child(1) .low').text(weather.forecast[0].low);
+    $('#spokane figure:nth-child(1) .high').text(weather.forecast[0].high);
+    $('#spokane figure:nth-child(1) img').attr('src', weather.forecast[0].image);
+	
+		// Display Data : Day 2
+    $('#spokane figure:nth-child(2) .next-day').text(weather.forecast[1].day);
+    $('#spokane figure:nth-child(2) .low').text(weather.forecast[1].low);
+    $('#spokane figure:nth-child(2) .high').text(weather.forecast[1].high);
+    $('#spokane figure:nth-child(2) img').attr('src', weather.forecast[1].image);
+		
+		// Display Data : Day 3
+    $('#spokane figure:nth-child(3) .next-day').text(weather.forecast[2].day);
+    $('#spokane figure:nth-child(3) .low').text(weather.forecast[2].low);
+    $('#spokane figure:nth-child(3) .high').text(weather.forecast[2].high);
+    $('#spokane figure:nth-child(3) img').attr('src', weather.forecast[2].image);
+		
+		// Display Data : Day 4
+    $('#spokane figure:nth-child(4) .next-day').text(weather.forecast[3].day);
+    $('#spokane figure:nth-child(4) .low').text(weather.forecast[3].low);
+    $('#spokane figure:nth-child(4) .high').text(weather.forecast[3].high);
+    $('#spokane figure:nth-child(4) img').attr('src', weather.forecast[3].image);
+
+    // Display Data : Day 5
+    $('#spokane figure:nth-child(5) .next-day').text(weather.forecast[4].day);
+    $('#spokane figure:nth-child(5) .low').text(weather.forecast[4].low);
+    $('#spokane figure:nth-child(5) .high').text(weather.forecast[4].high);
+    $('#spokane figure:nth-child(5) img').attr('src', weather.forecast[4].image);
+
 		// Change Icon w/ Condition code
       var iconCode = 'icon-' + weather.code;
-      $('#spokane i').attr('class', iconCode );
+	  var iconCodeDay1 = 'icon-' + weather.forecast[0].code;
+	  var iconCodeDay2 = 'icon-' + weather.forecast[1].code;
+	  var iconCodeDay3 = 'icon-' + weather.forecast[2].code;
+	  var iconCodeDay4 = 'icon-' + weather.forecast[3].code;
+	  var iconCodeDay5 = 'icon-' + weather.forecast[4].code;
+      
+		$('#spokane i:nth-child(1)').attr('class', iconCode );
+		$('.day1 i').attr('class', iconCodeDay1 );
+		$('.day2 i').attr('class', iconCodeDay2 );
+		$('.day3 i').attr('class', iconCodeDay3 );
+		$('.day4 i').attr('class', iconCodeDay4 );
+		$('.day5 i').attr('class', iconCodeDay5 );
 		
-    },
+	},
     error: function(error) {
       // Show if weather cannot be retreived
       console.log('Look outside.');
@@ -106,7 +147,7 @@ $.simpleWeather({
       $('#seattle .temp').text(weather.temp);
       $('#seattle .city').text(weather.city);
       $('#seattle img').attr('src', weather.image);
-      
+      $('#seattle .today').text(weather.forecast[0].day + ', ' + weather.forecast[0].date);
 		// Change Icon w/ Condition code
       var iconCode = 'icon-' + weather.code;
       $('#seattle i').attr('class', iconCode );
@@ -132,7 +173,7 @@ $.simpleWeather({
 		$('#portland .temp').text(weather.temp);
       $('#portland .city').text(weather.city);
       $('#portland img').attr('src', weather.image);
-      
+      $('#portland .today').text(weather.forecast[0].day + ', ' + weather.forecast[0].date);
 		// Change Icon w/ Condition code
       var iconCode = 'icon-' + weather.code;
       $('#portland i').attr('class', iconCode );
