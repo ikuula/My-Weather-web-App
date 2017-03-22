c5b4a67308a44c8e58e726e6037187965e259506
Isaac Kuula <ikuula90@gmail.com>
2017-03-21 17:56:43 -0700

    Get location works!
    Everything is working!
    BG changes based on code!

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,22 @@ $('#sidr-show-hide').sidr();
 // Close from inside
 $('#close').sidr({
   method: 'close'
-})
+});
 
 // Get and store Geo Location lat/long coordinates
+if ('geolocation' in navigator) {
+
+   $('.geo').show(); 
+
+} else {
+  
+  $('.geo').hide();
+  $('.geo').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+// Get Geo Location
+// Check for GeoLocation Support on Browser
 if ('geolocation' in navigator) {
 
    $('.geo').show(); 
@@ -39,33 +52,93 @@ $('.geo').click( function() {
    
 });
 
-//// Get Weather - Location
-//var getWeather = function(location) {
-//  
-//    $.simpleWeather({
-//    location: location,
-//    unit: 'f',
-//    success: function(weather) {
-//      
-//      // Entire weather object
-//      console.log(weather);
-//      
-//      // Display Data
-//      $('#currentLocation .temp').text(weather.temp);
-//      $('#currentLocation .city').text(weather.city);
-//      $('#currentLocation img').attr('src', weather.image);
-//      
-//		// Change Icon w/ Condition code
-//      var iconCode = 'icon-' + weather.code;
-//      $('i').attr('class', iconCode );
-//		
-//    },
-//    error: function(error) {
-//      // Show if weather cannot be retreived
-//      console.log('Look Outside.');
-//    }
-//  
-//  }); 
+// Show geolocated weather
+var getWeather = function(location) {
+    
+   $.simpleWeather({
+    location: location,
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+	  $('#currentLocation .currently').text(weather.currently);
+      $('#currentLocation .temp').text(weather.temp);
+      $('#currentLocation .city').text(weather.city);
+      $('#currentLocation img').attr('src', weather.image);
+	  $('#currentLocation .today').text(weather.forecast[0].day + ', ' + weather.forecast[0].date);
+	
+		// Display Data : Day 1
+    $('#currentLocation figure:nth-child(1) .low').text(weather.forecast[0].low);
+    $('#currentLocation figure:nth-child(1) .high').text(weather.forecast[0].high);
+    $('#currentLocation figure:nth-child(1) img').attr('src', weather.forecast[0].image);
+	
+		// Display Data : Day 2
+    $('#currentLocation figure:nth-child(2) .next-day').text(weather.forecast[1].day);
+    $('#currentLocation figure:nth-child(2) .low').text(weather.forecast[1].low);
+    $('#currentLocation figure:nth-child(2) .high').text(weather.forecast[1].high);
+    $('#currentLocation figure:nth-child(2) img').attr('src', weather.forecast[1].image);
+		
+		// Display Data : Day 3
+    $('#currentLocation figure:nth-child(3) .next-day').text(weather.forecast[2].day);
+    $('#currentLocation figure:nth-child(3) .low').text(weather.forecast[2].low);
+    $('#currentLocation figure:nth-child(3) .high').text(weather.forecast[2].high);
+    $('#currentLocation figure:nth-child(3) img').attr('src', weather.forecast[2].image);
+		
+		// Display Data : Day 4
+    $('#currentLocation figure:nth-child(4) .next-day').text(weather.forecast[3].day);
+    $('#currentLocation figure:nth-child(4) .low').text(weather.forecast[3].low);
+    $('#currentLocation figure:nth-child(4) .high').text(weather.forecast[3].high);
+    $('#currentLocation figure:nth-child(4) img').attr('src', weather.forecast[3].image);
+
+    // Display Data : Day 5
+    $('#currentLocation figure:nth-child(5) .next-day').text(weather.forecast[4].day);
+    $('#currentLocation figure:nth-child(5) .low').text(weather.forecast[4].low);
+    $('#currentLocation figure:nth-child(5) .high').text(weather.forecast[4].high);
+    $('#currentLocation figure:nth-child(5) img').attr('src', weather.forecast[4].image);
+
+		// Change Icon w/ Condition code
+      var currentLocationIconCode = 'icon-' + weather.code;
+	  var currentLocationIconCodeDay1 = 'icon-' + weather.forecast[0].code;
+	  var currentLocationIconCodeDay2 = 'icon-' + weather.forecast[1].code;
+	  var currentLocationIconCodeDay3 = 'icon-' + weather.forecast[2].code;
+	  var currentLocationIconCodeDay4 = 'icon-' + weather.forecast[3].code;
+	  var currentLocationIconCodeDay5 = 'icon-' + weather.forecast[4].code;
+      
+		$('#currentLocation i:nth-child(1)').attr('class', currentLocationIconCode );
+		$('#currentLocation .day1 i').attr('class', currentLocationIconCodeDay1 );
+		$('#currentLocation .day2 i').attr('class', currentLocationIconCodeDay2 );
+		$('#currentLocation .day3 i').attr('class', currentLocationIconCodeDay3 );
+		$('#currentLocation .day4 i').attr('class', currentLocationIconCodeDay4 );
+		$('#currentLocation .day5 i').attr('class', currentLocationIconCodeDay5 );
+	
+		if (weather.code >= 0 && weather.code <= 8) {
+          $('#currentLocation').addClass('bad');
+          }
+		if (weather.code >= 9 && weather.code <= 12) {
+          $('#currentLocation').addClass('wet');
+          }
+		if (weather.code >= 13 && weather.code <= 19) {
+          $('#currentLocation').addClass('snow');
+          }
+      if (weather.code >= 20 && weather.code <= 30) {
+          $('#currentLocation').addClass('sad');
+          }
+      if (weather.code >= 31 && weather.code <= 36) {
+		  $('#currentLocation').addClass('happy');
+          }
+		if (weather.code >= 37 && weather.code <= 47) {
+		  $('#currentLocation').addClass('bad');
+          }
+
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look Outside.');
+    }
+  
+  });
+    
+}; 
 	
 	// Spokane
 $.simpleWeather({
@@ -125,7 +198,26 @@ $.simpleWeather({
 		$('#spokane .day3 i').attr('class', iconCodeDay3 );
 		$('#spokane .day4 i').attr('class', iconCodeDay4 );
 		$('#spokane .day5 i').attr('class', iconCodeDay5 );
-		
+	
+		if (weather.code >= 0 && weather.code <= 8) {
+          $('#spokane').addClass('bad');
+          }
+		if (weather.code >= 9 && weather.code <= 12) {
+          $('#spokane').addClass('wet');
+          }
+		if (weather.code >= 13 && weather.code <= 19) {
+          $('#spokane').addClass('snow');
+          }
+      if (weather.code >= 20 && weather.code <= 30) {
+          $('#spokane').addClass('sad');
+          }
+      if (weather.code >= 31 && weather.code <= 36) {
+		  $('#spokane').addClass('happy');
+          }
+		if (weather.code >= 37 && weather.code <= 47) {
+		  $('#spokane').addClass('bad');
+          }
+      
 	},
 	
     error: function(error) {
@@ -194,6 +286,24 @@ $.simpleWeather({
 		$('#seattle .day4 i').attr('class', seattleIconCodeDay4 );
 		$('#seattle .day5 i').attr('class', seattleIconCodeDay5 );
 		
+		if (weather.code >= 0 && weather.code <= 8) {
+          $('#seattle').addClass('bad');
+          }
+		if (weather.code >= 9 && weather.code <= 12) {
+          $('#seattle').addClass('wet');
+          }
+		if (weather.code >= 13 && weather.code <= 19) {
+          $('#seattle').addClass('snow');
+          }
+      if (weather.code >= 20 && weather.code <= 30) {
+          $('#seattle').addClass('sad');
+          }
+      if (weather.code >= 31 && weather.code <= 36) {
+		  $('#seattle').addClass('happy');
+          }
+		if (weather.code >= 37 && weather.code <= 47) {
+		  $('#seattle').addClass('bad');
+          }
 	},
     error: function(error) {
       // Show if weather cannot be retreived
@@ -261,6 +371,112 @@ $.simpleWeather({
 		$('#portland .day4 i').attr('class', portlandIconCodeDay4 );
 		$('#portland .day5 i').attr('class', portlandIconCodeDay5 );
 		
+		if (weather.code >= 0 && weather.code <= 8) {
+          $('#portland').addClass('bad');
+          }
+		if (weather.code >= 9 && weather.code <= 12) {
+          $('#portland').addClass('wet');
+          }
+		if (weather.code >= 13 && weather.code <= 19) {
+          $('#portland').addClass('snow');
+          }
+      if (weather.code >= 20 && weather.code <= 30) {
+          $('#portland').addClass('sad');
+          }
+      if (weather.code >= 31 && weather.code <= 36) {
+		  $('#portland').addClass('happy');
+          }
+		if (weather.code >= 37 && weather.code <= 47) {
+		  $('#portland').addClass('bad');
+          }
+	},
+	
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
+
+// Ventura
+$.simpleWeather({
+    location: 93003,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+	  $('#ventura .currently').text(weather.currently);
+      $('#ventura .temp').text(weather.temp);
+      $('#ventura .city').text(weather.city);
+      $('#ventura img').attr('src', weather.image);
+	  $('#ventura .today').text(weather.forecast[0].day + ', ' + weather.forecast[0].date);
+	
+		// Display Data : Day 1
+    $('#ventura figure:nth-child(1) .low').text(weather.forecast[0].low);
+    $('#ventura figure:nth-child(1) .high').text(weather.forecast[0].high);
+    $('#ventura figure:nth-child(1) img').attr('src', weather.forecast[0].image);
+	
+		// Display Data : Day 2
+    $('#ventura figure:nth-child(2) .next-day').text(weather.forecast[1].day);
+    $('#ventura figure:nth-child(2) .low').text(weather.forecast[1].low);
+    $('#ventura figure:nth-child(2) .high').text(weather.forecast[1].high);
+    $('#ventura figure:nth-child(2) img').attr('src', weather.forecast[1].image);
+		
+		// Display Data : Day 3
+    $('#ventura figure:nth-child(3) .next-day').text(weather.forecast[2].day);
+    $('#ventura figure:nth-child(3) .low').text(weather.forecast[2].low);
+    $('#ventura figure:nth-child(3) .high').text(weather.forecast[2].high);
+    $('#ventura figure:nth-child(3) img').attr('src', weather.forecast[2].image);
+		
+		// Display Data : Day 4
+    $('#ventura figure:nth-child(4) .next-day').text(weather.forecast[3].day);
+    $('#ventura figure:nth-child(4) .low').text(weather.forecast[3].low);
+    $('#ventura figure:nth-child(4) .high').text(weather.forecast[3].high);
+    $('#ventura figure:nth-child(4) img').attr('src', weather.forecast[3].image);
+
+    // Display Data : Day 5
+    $('#ventura figure:nth-child(5) .next-day').text(weather.forecast[4].day);
+    $('#ventura figure:nth-child(5) .low').text(weather.forecast[4].low);
+    $('#ventura figure:nth-child(5) .high').text(weather.forecast[4].high);
+    $('#ventura figure:nth-child(5) img').attr('src', weather.forecast[4].image);
+
+		// Change Icon w/ Condition code
+      var venturaIconCode = 'icon-' + weather.code;
+	  var venturaIconCodeDay1 = 'icon-' + weather.forecast[0].code;
+	  var venturaIconCodeDay2 = 'icon-' + weather.forecast[1].code;
+	  var venturaIconCodeDay3 = 'icon-' + weather.forecast[2].code;
+	  var venturaIconCodeDay4 = 'icon-' + weather.forecast[3].code;
+	  var venturaIconCodeDay5 = 'icon-' + weather.forecast[4].code;
+      
+		$('#ventura i:nth-child(1)').attr('class', venturaIconCode );
+		$('#ventura .day1 i').attr('class', venturaIconCodeDay1 );
+		$('#ventura .day2 i').attr('class', venturaIconCodeDay2 );
+		$('#ventura .day3 i').attr('class', venturaIconCodeDay3 );
+		$('#ventura .day4 i').attr('class', venturaIconCodeDay4 );
+		$('#ventura .day5 i').attr('class', venturaIconCodeDay5 );
+ 
+		//get condition code
+      
+		if (weather.code >= 0 && weather.code <= 8) {
+          $('#ventura').addClass('bad');
+          }
+		if (weather.code >= 9 && weather.code <= 12) {
+          $('#ventura').addClass('wet');
+          }
+		if (weather.code >= 13 && weather.code <= 19) {
+          $('#ventura').addClass('snow');
+          }
+      if (weather.code >= 20 && weather.code <= 30) {
+          $('#ventura').addClass('sad');
+          }
+      if (weather.code >= 31 && weather.code <= 36) {
+		  $('#ventura').addClass('happy');
+          }
+		if (weather.code >= 37 && weather.code <= 47) {
+		  $('#ventura').addClass('bad');
+          }
 	},
 	
     error: function(error) {
@@ -270,5 +486,178 @@ $.simpleWeather({
   
   });
 	
+// Vegas
+$.simpleWeather({
+    location: 89101,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+	  $('#vegas .currently').text(weather.currently);
+      $('#vegas .temp').text(weather.temp);
+      $('#vegas .city').text(weather.city);
+      $('#vegas img').attr('src', weather.image);
+	  $('#vegas .today').text(weather.forecast[0].day + ', ' + weather.forecast[0].date);
+	
+		// Display Data : Day 1
+    $('#vegas figure:nth-child(1) .low').text(weather.forecast[0].low);
+    $('#vegas figure:nth-child(1) .high').text(weather.forecast[0].high);
+    $('#vegas figure:nth-child(1) img').attr('src', weather.forecast[0].image);
+	
+		// Display Data : Day 2
+    $('#vegas figure:nth-child(2) .next-day').text(weather.forecast[1].day);
+    $('#vegas figure:nth-child(2) .low').text(weather.forecast[1].low);
+    $('#vegas figure:nth-child(2) .high').text(weather.forecast[1].high);
+    $('#vegas figure:nth-child(2) img').attr('src', weather.forecast[1].image);
+		
+		// Display Data : Day 3
+    $('#vegas figure:nth-child(3) .next-day').text(weather.forecast[2].day);
+    $('#vegas figure:nth-child(3) .low').text(weather.forecast[2].low);
+    $('#vegas figure:nth-child(3) .high').text(weather.forecast[2].high);
+    $('#vegas figure:nth-child(3) img').attr('src', weather.forecast[2].image);
+		
+		// Display Data : Day 4
+    $('#vegas figure:nth-child(4) .next-day').text(weather.forecast[3].day);
+    $('#vegas figure:nth-child(4) .low').text(weather.forecast[3].low);
+    $('#vegas figure:nth-child(4) .high').text(weather.forecast[3].high);
+    $('#vegas figure:nth-child(4) img').attr('src', weather.forecast[3].image);
 
+    // Display Data : Day 5
+    $('#vegas figure:nth-child(5) .next-day').text(weather.forecast[4].day);
+    $('#vegas figure:nth-child(5) .low').text(weather.forecast[4].low);
+    $('#vegas figure:nth-child(5) .high').text(weather.forecast[4].high);
+    $('#vegas figure:nth-child(5) img').attr('src', weather.forecast[4].image);
 
+		// Change Icon w/ Condition code
+      var vegasIconCode = 'icon-' + weather.code;
+	  var vegasIconCodeDay1 = 'icon-' + weather.forecast[0].code;
+	  var vegasIconCodeDay2 = 'icon-' + weather.forecast[1].code;
+	  var vegasIconCodeDay3 = 'icon-' + weather.forecast[2].code;
+	  var vegasIconCodeDay4 = 'icon-' + weather.forecast[3].code;
+	  var vegasIconCodeDay5 = 'icon-' + weather.forecast[4].code;
+      
+		$('#vegas i:nth-child(1)').attr('class', vegasIconCode );
+		$('#vegas .day1 i').attr('class', vegasIconCodeDay1 );
+		$('#vegas .day2 i').attr('class', vegasIconCodeDay2 );
+		$('#vegas .day3 i').attr('class', vegasIconCodeDay3 );
+		$('#vegas .day4 i').attr('class', vegasIconCodeDay4 );
+		$('#vegas .day5 i').attr('class', vegasIconCodeDay5 );
+ 
+		//get condition code
+      
+		if (weather.code >= 0 && weather.code <= 8) {
+          $('#vegas').addClass('bad');
+          }
+		if (weather.code >= 9 && weather.code <= 12) {
+          $('#vegas').addClass('wet');
+          }
+		if (weather.code >= 13 && weather.code <= 19) {
+          $('#vegas').addClass('snow');
+          }
+      if (weather.code >= 20 && weather.code <= 30) {
+          $('#vegas').addClass('sad');
+          }
+      if (weather.code >= 31 && weather.code <= 36) {
+		  $('#vegas').addClass('happy');
+          }
+		if (weather.code >= 37 && weather.code <= 47) {
+		  $('#vegas').addClass('bad');
+          }
+	},
+	
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
+
+// phoenix
+$.simpleWeather({
+    location: 85001,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+	  $('#phoenix .currently').text(weather.currently);
+      $('#phoenix .temp').text(weather.temp);
+      $('#phoenix .city').text(weather.city);
+      $('#phoenix img').attr('src', weather.image);
+	  $('#phoenix .today').text(weather.forecast[0].day + ', ' + weather.forecast[0].date);
+	
+		// Display Data : Day 1
+    $('#phoenix figure:nth-child(1) .low').text(weather.forecast[0].low);
+    $('#phoenix figure:nth-child(1) .high').text(weather.forecast[0].high);
+    $('#phoenix figure:nth-child(1) img').attr('src', weather.forecast[0].image);
+	
+		// Display Data : Day 2
+    $('#phoenix figure:nth-child(2) .next-day').text(weather.forecast[1].day);
+    $('#phoenix figure:nth-child(2) .low').text(weather.forecast[1].low);
+    $('#phoenix figure:nth-child(2) .high').text(weather.forecast[1].high);
+    $('#phoenix figure:nth-child(2) img').attr('src', weather.forecast[1].image);
+		
+		// Display Data : Day 3
+    $('#phoenix figure:nth-child(3) .next-day').text(weather.forecast[2].day);
+    $('#phoenix figure:nth-child(3) .low').text(weather.forecast[2].low);
+    $('#phoenix figure:nth-child(3) .high').text(weather.forecast[2].high);
+    $('#phoenix figure:nth-child(3) img').attr('src', weather.forecast[2].image);
+		
+		// Display Data : Day 4
+    $('#phoenix figure:nth-child(4) .next-day').text(weather.forecast[3].day);
+    $('#phoenix figure:nth-child(4) .low').text(weather.forecast[3].low);
+    $('#phoenix figure:nth-child(4) .high').text(weather.forecast[3].high);
+    $('#phoenix figure:nth-child(4) img').attr('src', weather.forecast[3].image);
+
+    // Display Data : Day 5
+    $('#phoenix figure:nth-child(5) .next-day').text(weather.forecast[4].day);
+    $('#phoenix figure:nth-child(5) .low').text(weather.forecast[4].low);
+    $('#phoenix figure:nth-child(5) .high').text(weather.forecast[4].high);
+    $('#phoenix figure:nth-child(5) img').attr('src', weather.forecast[4].image);
+
+		// Change Icon w/ Condition code
+      var phoenixIconCode = 'icon-' + weather.code;
+	  var phoenixIconCodeDay1 = 'icon-' + weather.forecast[0].code;
+	  var phoenixIconCodeDay2 = 'icon-' + weather.forecast[1].code;
+	  var phoenixIconCodeDay3 = 'icon-' + weather.forecast[2].code;
+	  var phoenixIconCodeDay4 = 'icon-' + weather.forecast[3].code;
+	  var phoenixIconCodeDay5 = 'icon-' + weather.forecast[4].code;
+      
+		$('#phoenix i:nth-child(1)').attr('class', phoenixIconCode );
+		$('#phoenix .day1 i').attr('class', phoenixIconCodeDay1 );
+		$('#phoenix .day2 i').attr('class', phoenixIconCodeDay2 );
+		$('#phoenix .day3 i').attr('class', phoenixIconCodeDay3 );
+		$('#phoenix .day4 i').attr('class', phoenixIconCodeDay4 );
+		$('#phoenix .day5 i').attr('class', phoenixIconCodeDay5 );
+ 
+		//get condition code
+      
+		if (weather.code >= 0 && weather.code <= 8) {
+          $('#phoenix').addClass('bad');
+          }
+		if (weather.code >= 9 && weather.code <= 12) {
+          $('#phoenix').addClass('wet');
+          }
+		if (weather.code >= 13 && weather.code <= 19) {
+          $('#phoenix').addClass('snow');
+          }
+      if (weather.code >= 20 && weather.code <= 30) {
+          $('#phoenix').addClass('sad');
+          }
+      if (weather.code >= 31 && weather.code <= 36) {
+		  $('#phoenix').addClass('happy');
+          }
+		if (weather.code >= 37 && weather.code <= 47) {
+		  $('#phoenix').addClass('bad');
+          }
+	},
+	
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
